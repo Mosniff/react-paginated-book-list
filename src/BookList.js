b57a6a940b1a018e8f78b59fc86a137035f922e8
Joe Hill <joe.ismail.hill@gmail.com>
2020-07-20 18:12:23 +0100

    Added pagination

--- src/BookList.js
+++ src/BookList.js
@@ -4,35 +4,62 @@ import api from './api/api';
 import {
   Container,
   Jumbotron,
-  Card
+  Card,
+  Row,
+  Col,
+  Button
 } from 'react-bootstrap';
 
 const BookList = () => {
   const [data, setData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    api.books.apiBooks({ }).then(response => setData(response));
+    api.books.apiBooks({ page: currentPage }).then(response => setData(response));
   });
 
   return (
     <Container>
       <Jumbotron>
         <h1 className="h1 text-center">Book List</h1>
+        <h3 className="h3 text-center">Page {currentPage}</h3>
       </Jumbotron>
-      {data
-          ? data.items.books.map(book => (
-              <Card>
-                <Card.Body>
-                < Card.Title>{book.book_title}</Card.Title>
-                  <p>Book author: {book.book_author}</p>
-                  <p>Book pages: {book.book_pages}</p>
-                  <p>Publication year: {book.book_publication_year}</p>
-                  <p>Publication country: {book.book_publication_country}</p>
-                  <p>Publication city: {book.book_publication_city}</p>
-                </Card.Body>
-              </Card>
-          )
-        ) : null}
+      <Row>
+        <Col xs="12" className="d-flex justify-content-end">
+          <Button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="mr-1 flex-fill"
+          >
+            Prev Page
+          </Button>
+          <Button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={data ? data.moreItems : false}
+            className="flex-fill"
+          >
+            Next Page
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        {data
+            ? data.items.books.map(book => (
+              <Col xs="12" lg="12" className="mb-1">
+                <Card>
+                  <Card.Body>
+                  < Card.Title>{book.book_title}</Card.Title>
+                    <p>Book author: {book.book_author}</p>
+                    <p>Book pages: {book.book_pages}</p>
+                    <p>Publication year: {book.book_publication_year}</p>
+                    <p>Publication country: {book.book_publication_country}</p>
+                    <p>Publication city: {book.book_publication_city}</p>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          ) : null}
+        </Row>
     </Container>
   )
 }
--- src/api/services/books.js
+++ src/api/services/books.js
@@ -1,8 +1,11 @@
 import { client } from '../api';
 
-const apiBooks = async() => {
-  const response = await client.post('/books');
-  return { items: response.data };
+const apiBooks = async({
+  page = 1,
+  itemsPerPage = 20
+}) => {
+  const response = await client.post('/books', { page, itemsPerPage });
+  return { items: response.data, moreItems: (itemsPerPage * page >= response.data.count) };
 }
 
 export default { apiBooks };
